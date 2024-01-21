0b57a89cd4b10b93b7c2f26e12b8b8ae25d36973
emmanueliyanu21 <emmanueliyanu2012@gmail.com>
2024-01-21 14:39:29 +0100

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
-import Head from 'next/head';
 
 import './globals.css'
-import ThemeClient from './rootLayout'
+import ThemeClient from './themeClient'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
@@ -21,11 +20,6 @@ export default function RootLayout({
   return (
     
     <html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-      </Head>
       <body>
         <ThemeClient>
           <Header />
